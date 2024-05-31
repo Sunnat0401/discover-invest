419f3bbd12ed28ac59957feb86d5eefc0185d1d9
sunnatjon <sunnattoxtaqulov4444@gmail.com>
2024-05-31 10:34:26 +0300

    ourproject file finished

--- src/Components/OurProjects/OurProjects.jsx
+++ src/Components/OurProjects/OurProjects.jsx
@@ -8,7 +8,27 @@ const OurProjects = () => {
           <span className='pl'>   Loyihalar</span>
         </h2>
         <div className="ourproject-cards">
-            <div className="ourproject-card background1">
+            <div className="ourproject-card">
+            <div class="overlay"></div>
+             <div className="ourproject-info">
+             <h3 className='ourproject-headline'>Magic City</h3>
+              <p className="ourproject-text">
+              Shaharning yuragida barpo etilgan turar joy mavzesi.
+              </p>
+             </div>
+            </div>
+            <div className="ourproject-card  background1">
+            <div className="ourproject-info">
+             <h3 className='ourproject-headline'>Boulevard</h3>
+              <p className="ourproject-text">
+              Aholi qulayligini ta’minlash maqsadida bunyod qilingan ko’p vazifali turar joy mavzesi
+              </p>
+             </div>
+            </div>
+        </div>
+        <div className="ourproject-cards ">
+            <div className="ourproject-card  background2">
+            <div class="overlay"></div>
              <div className="ourproject-info">
              <h3 className='ourproject-headline'>Gardens Residence</h3>
               <p className="ourproject-text">
@@ -16,7 +36,37 @@ const OurProjects = () => {
               </p>
              </div>
             </div>
-            <div className="ourproject-card"></div>
+            <div className="ourproject-card  background3">
+            <div className="ourproject-info">
+             <h3 className='ourproject-headline'>Fonon</h3>
+              <p className="ourproject-text">
+              Shaharning yuragida barpo etilgan turar joy mavzesi.
+              </p>
+             </div>
+            </div>
+        </div>
+        <div className="ourproject-cards">
+            <div className="ourproject-card  background4">
+            <div class="overlay"></div>
+             <div className="ourproject-info">
+             <h3 className='ourproject-headline'>Invento</h3>
+              <p className="ourproject-text">
+              Tashkent City biznes markazida joylashgan xalqaro maktab va bolalar bog’chasi.
+              </p>
+             </div>
+            </div>
+            <div className="ourproject-card  background5">
+            <div className="ourproject-info">
+             <h3 className='ourproject-headline'>Prezident maktabi</h3>
+              <p className="ourproject-text">
+              Bu xalqaro standartlarga javob beradigan bilim beruvchi ta’lim maskanidir
+              </p>
+             </div>
+            </div>
+        </div>
+        <div className="ourproject-buttons">
+         <a href='' className="ourproject-button"></a>
+         <a href='' className="ourproject-button active ">Barcha loyihalar</a>
         </div>
         </div>
     </div>
